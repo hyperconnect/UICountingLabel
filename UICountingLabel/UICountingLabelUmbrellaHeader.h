2c76a1e79d5f64d853573a5de3fce1972b641ba1
dgoon <kslee@hpcnt.com>
2015-06-01 15:24:58 +0900

    프레임웍 빌드용 프로젝트 생성

---
+++ UICountingLabel/UICountingLabelUmbrellaHeader.h
@@ -0,0 +1,20 @@
+//
+//  UICountingLabel.h
+//  UICountingLabel
+//
+//  Created by dgoon on 2015. 6. 1..
+//  Copyright (c) 2015년 Hyperconnect. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for UICountingLabel.
+FOUNDATION_EXPORT double UICountingLabelVersionNumber;
+
+//! Project version string for UICountingLabel.
+FOUNDATION_EXPORT const unsigned char UICountingLabelVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UICountingLabel/PublicHeader.h>
+
+
+#import "UICountingLabel.h"
